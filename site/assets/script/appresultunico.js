a4aa63293c93bae29ed094c6e249e4b4b6bcc613
Gabriel Mayer Clary <gabrielm05.clary@gmail.com>
2024-05-15 16:18:32 -0300

    Desenvolvimento pagina Resultado Unico

--- site/assets/script/appresultunico.js
+++ site/assets/script/appresultunico.js
@@ -1,60 +1,50 @@
 
+
+
 const ctx = document.getElementById('pie-chart1');
 
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
-
-
-/*new Chart(document.getElementById('pie-chart1'), {
-    type: 'pie',
-    data: {
-        labels : ["HTML", "CSS", "JAVASCRIPT", "PHP", "PYTHON"],
-        datasets: [{
-            backgroundColor : ["#e63946", "#254BDD",
-            "#ffbe0b", "#1d3557", "#326998"],
-            data: [418, 263, 464,586, 332]
-        }]
-    },
-    options: {
-        title: {
-            display: true,
-            text: 'Pie chart for result'
-        },
-        responsive: true
-    }
+new Chart(ctx, {
+  type: 'pie',
+  data: {
+    labels: [
+      'Gasto A',
+      'Gasto B',
+      'Gasto C'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  }
 });
 
-new Chart(document.getElementById('pie-chart1'), {
-    type: 'pie',
-    data: {
-        labels : ["HTML", "CSS", "JAVASCRIPT", "PHP", "PYTHON"],
-        datasets: [{
-            backgroundColor : ["#e63946", "#254BDD",
-            "#ffbe0b", "#1d3557", "#326998"],
-            data: [418, 263, 464,586, 332]
-        }]
-    },
-    options: {
-        title: {
-            display: true,
-            text: 'Pie chart for result'
-        },
-        responsive: true
-    }
-});*/
+
+const ctx2 = document.getElementById('pie-chart2');
+
+new Chart(ctx2, {
+  type: 'pie',
+  data: {
+    labels: [
+      'Gasto A',
+      'Gasto B',
+      'Gasto C'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  }
+});
+
